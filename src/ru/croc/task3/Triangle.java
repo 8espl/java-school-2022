21f9ba6e81f2e97c5d2eaab2efb877905f00cf21
Elvina <pale.espl@gmail.com>
2022-10-26 19:49:05 +0300

    Добавила решение задачи №3

--- src/ru/croc/task3/Triangle.java
+++ src/ru/croc/task3/Triangle.java
@@ -1,17 +1,25 @@
 package ru.croc.task3;
 
+import java.util.Scanner;
+
 public class Triangle {
+
     static class Point {
         double x;
         double y;
     }
 
-    public static double square(){
+    /*
+     * Вычисляет площадь треугольника по трем точкам
+     * с помощью свойств векторного произведения
+     */
+    public static double square(Point a, Point b, Point c) {
 
-        double s = 0.;
+        double s;
+
+        s = Math.abs(((b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x))) / 2.;
 
         return s;
-
     }
 
     public static void main(String[] args) {
@@ -28,7 +36,17 @@ public class Triangle {
         c.x = 0.0;
         c.y = 2.0;
 
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Введите координаты вершин треугольника");
+        a.x = sc.nextDouble();
+        a.y = sc.nextDouble();
+        b.x = sc.nextDouble();
+        b.y = sc.nextDouble();
+        c.x = sc.nextDouble();
+        c.y = sc.nextDouble();
+
+        System.out.println("Площадь треугольника: " + square(a, b, c));
     }
 }
 
