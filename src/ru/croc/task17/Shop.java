ab49f288ce23e65d89c19d7f3c81c8b48437e299
Elvina <pale.espl@gmail.com>
2022-12-12 05:16:58 +0300

    Переименовала сущности

--- src/ru/croc/task17/Shop.java
+++ src/ru/croc/task17/Shop.java
@@ -2,7 +2,7 @@ package ru.croc.task17;
 
 public class Shop {
     public static void main(String[] args) {
-        args = new String[] {"src/ru/croc/task17/data.CSV"};
+        //args = new String[] {"src/ru/croc/task17/data.CSV"};
         String connectionURL = "jdbc:h2:~/Shop";
         String user = "sa";
         String password = "";
